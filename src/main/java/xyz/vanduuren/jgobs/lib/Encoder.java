c4fe065dfae1f37f9d3bd51cf08ecd3d3224ebb9
Boy van Duuren <bcvanduuren@gmail.com>
2016-06-22 23:21:13 +0200

    Added unsigned integer encoding + tests

---
+++ src/main/java/xyz/vanduuren/jgobs/lib/Encoder.java
@@ -0,0 +1,38 @@
+package xyz.vanduuren.jgobs.lib;
+
+/**
+ * Encoder encodes a Java object to Go's gob encoding
+ *
+ * @author Boy van Duuren <boy@vanduuren.xyz>
+ * @since 2016-06-22
+ */
+public class Encoder {
+
+    /**
+     * Encode an unsigned integer (max 64 bits) to gob
+     * @param value The value to encode
+     * @return A gob encoded byte array representing the value
+     */
+    private static byte[] encodeUnsignedInteger(long value) {
+        byte[] encodedUnsignedInteger;
+        // If value < 128 return it as a byte value
+        if (Long.compareUnsigned(value, 128) < 0) {
+            encodedUnsignedInteger = new byte[]{(byte)value};
+        } else {
+            // Start encoding the value if it's >= 128
+            // First, we set the length, negated
+            byte amountOfBytes = (byte)((int)Math.ceil(Long.toBinaryString(value).length()/(double)8));
+            encodedUnsignedInteger = new byte[amountOfBytes+1];
+
+            // Start with the length of the integer, negated
+            encodedUnsignedInteger[0] = (byte)(amountOfBytes-1^255);
+            for (int i = 0; i < amountOfBytes; i++) {
+                // Add the integer values to our byte array, big-endian
+                encodedUnsignedInteger[i+1] = (byte)(value >> (amountOfBytes-i-1)*8);
+            }
+        }
+
+        return encodedUnsignedInteger;
+    }
+
+}
---
+++ src/test/java/xyz/vanduuren/jgobs/lib/EncoderTest.java
@@ -0,0 +1,68 @@
+package xyz.vanduuren.jgobs.lib;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Give a description of EncoderTest here.
+ *
+ * @author Boy van Duuren <boy@vanduuren.xyz>
+ * @since 2016-06-22
+ */
+public class EncoderTest {
+
+    private static Method encodeUnsignedInteger;
+
+    private static byte[] stringToByteArray(String s) {
+        return DatatypeConverter.parseHexBinary(s);
+    }
+
+    @BeforeClass
+    public static void setupMethods() {
+        try {
+            encodeUnsignedInteger =
+                    Encoder.class.getDeclaredMethod("encodeUnsignedInteger", Long.TYPE);
+            encodeUnsignedInteger.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testUnsignedIntegerEncoding() throws Exception {
+        if (encodeUnsignedInteger != null) {
+            byte[] retValue;
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 256));
+            assertArrayEquals(stringToByteArray("fe0100"), retValue);
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 54738378));
+            assertArrayEquals(stringToByteArray("fc03433dca"), retValue);
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 8002336222354889572L));
+            assertArrayEquals(stringToByteArray("f86f0e026562b45b64"), retValue);
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 634035792414663034L));
+            assertArrayEquals(stringToByteArray("f808cc8c31a5c2617a"), retValue);
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 9));
+            assertArrayEquals(stringToByteArray("09"), retValue);
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 127));
+            assertArrayEquals(stringToByteArray("7f"), retValue);
+
+            retValue = (byte[])(encodeUnsignedInteger.invoke(Encoder.class, 128));
+            assertArrayEquals(stringToByteArray("ff80"), retValue);
+        } else {
+            fail("Error while getting Encoder.encodeUnsignedInteger using reflection.");
+        }
+    }
+
+}
