0daed44a0cf2de8c710187a68a072a06e2cd691e
Boy van Duuren <bcvanduuren@gmail.com>
2016-06-23 22:13:21 +0200

    Added signed integer encoding

--- src/main/java/xyz/vanduuren/jgobs/lib/Encoder.java
+++ src/main/java/xyz/vanduuren/jgobs/lib/Encoder.java
@@ -1,5 +1,7 @@
 package xyz.vanduuren.jgobs.lib;
 
+import java.util.Arrays;
+
 /**
  * Encoder encodes a Java object to Go's gob encoding
  *
@@ -7,6 +9,34 @@ package xyz.vanduuren.jgobs.lib;
  * @since 2016-06-22
  */
 public class Encoder {
+
+    /**
+     * Convert a long to a byte array
+     * @param value The value to be converted
+     * @param amountOfBytes The amount of bytes we need to encode the long
+     * @return The converted value as byte array
+     */
+    private static byte[] longToByteArray(long value, int amountOfBytes) {
+        byte[] result = new byte[amountOfBytes];
+
+        for (int i = 0; i < amountOfBytes; i++) {
+            result[i] = (byte)(value >> (amountOfBytes-i-1)*8);
+        }
+
+        return result;
+    }
+
+    /**
+     * Concatenate two byte arrays
+     * @param a The first byte array
+     * @param b The second byte array
+     * @return The concatenated byte array
+     */
+    private static byte[] concatByteArrays(byte[] a, byte[] b) {
+        byte[] result = Arrays.copyOf(a, a.length + b.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
+    }
 
     /**
      * Encode an unsigned integer (max 64 bits) to gob
@@ -20,22 +50,37 @@ public class Encoder {
             encodedUnsignedInteger = new byte[]{(byte)value};
         } else {
             // Start encoding the value if it's >= 128
-            // First, we'll need the amount of bytes needed to represent this value
+            // Calculate the amount of bytes
             byte amountOfBytes = (byte)((int)Math.ceil(Long.toBinaryString(value).length()/(double)8));
-            // Create a minimal length byte array
-            encodedUnsignedInteger = new byte[amountOfBytes+1];
-
-            // Start with the size of the integer in bytes, negated
-            encodedUnsignedInteger[0] = (byte)(amountOfBytes-1^255);
-            for (int i = 0; i < amountOfBytes; i++) {
-                // Add the integer values to our byte array, big-endian
-                encodedUnsignedInteger[i+1] = (byte)(value >> (amountOfBytes-i-1)*8);
-            }
+            // Create the encoded result, first byte is the amount of bytes, negated
+            encodedUnsignedInteger = concatByteArrays(new byte[]{(byte)(amountOfBytes-1^255)}, longToByteArray(value, amountOfBytes));
         }
 
         return encodedUnsignedInteger;
     }
 
+    /**
+     * Encode a long value as a signed integer (max 64 bits)
+     * @param value The value to encode
+     * @return A gob encoded byte array representing the value
+     */
+    public static byte[] encodeSignedInteger(long value) {
+        long encoded;
+        // if value < 0 then complement, shift left and set LSB to 1
+        if (value < 0) {
+            encoded = (~value << 1) | 1;
+        } else {
+            encoded =  value << 1;
+        }
+
+        return encodeUnsignedInteger(encoded);
+    }
+
+    /**
+     * Encode a boolean
+     * @param value The value to encode
+     * @return A gob encoded byte array representing the value
+     */
     public static byte[] encodeBoolean(boolean value) {
         if (value) {
             return encodeUnsignedInteger(1);
--- src/test/java/xyz/vanduuren/jgobs/lib/EncoderTest.java
+++ src/test/java/xyz/vanduuren/jgobs/lib/EncoderTest.java
@@ -43,6 +43,28 @@ public class EncoderTest {
     }
 
     @Test
+    public void testSignedIntegerEncoding() {
+        Map<Long, String> testValues = new HashMap<>();
+
+        testValues.put(12345L, "fe6072");
+        testValues.put(-12345L, "fe6071");
+        testValues.put(-129L, "fe0101");
+        testValues.put(549287634987L, "fbffc8305056");
+        testValues.put(-4593287429872L, "fa085aea87fddf");
+        testValues.put(-3459823598723459780L, "f860078676d6d9d587");
+        testValues.put(2345098723549807L, "f910a9b5b0c2c4de");
+        testValues.put(-9223372036854775808L, "f8ffffffffffffffff");
+        testValues.put(9223372036854775807L, "f8fffffffffffffffe");
+
+        testValues.entrySet().stream()
+                .forEach(testEntry -> {
+                    byte[] retValue = new byte[0];
+                    retValue = Encoder.encodeSignedInteger(testEntry.getKey());
+                    assertArrayEquals(stringToByteArray(testEntry.getValue()), retValue);
+                });
+    }
+
+    @Test
     public void testBooleanEncoding() {
         assertArrayEquals(stringToByteArray("00"), Encoder.encodeBoolean(false));
         assertArrayEquals(stringToByteArray("01"), Encoder.encodeBoolean(true));
