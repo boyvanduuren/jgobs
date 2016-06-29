008af8f06cd3cd20626edf9cbfdd22b015038a50
Boy van Duuren <bcvanduuren@gmail.com>
2016-06-29 21:35:00 +0200

    Wrote encoding method for CommonType

--- src/main/java/xyz/vanduuren/jgobs/types/CommonType.java
+++ src/main/java/xyz/vanduuren/jgobs/types/CommonType.java
@@ -1,5 +1,9 @@
 package xyz.vanduuren.jgobs.types;
 
+import xyz.vanduuren.jgobs.lib.BasicEncoder;
+import xyz.vanduuren.jgobs.lib.ByteArrayUtilities;
+
+import java.io.UnsupportedEncodingException;
 import java.util.AbstractMap;
 
 /**
@@ -9,9 +13,6 @@ import java.util.AbstractMap;
  * @since 2016-06-29
  */
 public class CommonType extends GobCompositeType<AbstractMap.SimpleEntry<String, Integer>> {
-
-    private String name;
-    private int id;
 
     public CommonType(AbstractMap.SimpleEntry<String, Integer> nameAndId) {
         super(nameAndId);
@@ -24,7 +25,22 @@ public class CommonType extends GobCompositeType<AbstractMap.SimpleEntry<String,
 
     @Override
     public byte[] encode() {
-        return new byte[0];
+        try {
+            byte[] encodedCommonType;
+            byte[] encodedName = BasicEncoder.encodeString(unEncodedData.getKey());
+            byte[] encodedId = BasicEncoder.encodeSignedInteger(unEncodedData.getValue());
+            byte[] nullByteArray = new byte[]{(byte) 0};
+            byte[] oneByteArray = new byte[]{(byte) 1};
+
+            encodedCommonType = ByteArrayUtilities.concat(oneByteArray, encodedName,
+                    oneByteArray, encodedId, nullByteArray);
+
+            return encodedCommonType;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error while encoding " + this.getClass().getName()
+                    + ", couldn't encode name using UTF-8.");
+        }
     }
 
 }
---
+++ src/test/java/xyz/vanduuren/jgobs/types/CommonTypeTest.java
@@ -0,0 +1,39 @@
+package xyz.vanduuren.jgobs.types;
+
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Test the encoding and decoding for CommonType.
+ *
+ * @author Boy van Duuren <boy@vanduuren.xyz>
+ * @since 2016-06-29
+ */
+public class CommonTypeTest {
+    @Test
+    public void encode() throws Exception {
+        new CommonType(new AbstractMap.SimpleEntry<>("Point", 65));
+        Map<CommonType, String> testValues = new HashMap<>();
+
+        testValues.put(new CommonType(new AbstractMap.SimpleEntry<>("Point", 65)),
+                "0105506f696e7401ff8200");
+        testValues.put(new CommonType(new AbstractMap.SimpleEntry<>("Baz", 65)),
+                "010342617a01ff8200");
+        testValues.put(new CommonType(new AbstractMap.SimpleEntry<>("Foo", 66)),
+                "0103466f6f01ff8400");
+
+
+        testValues.entrySet().stream()
+                .forEach(testEntry -> {
+                    byte[] retValue = testEntry.getKey().encode();
+                    assertArrayEquals(DatatypeConverter.parseHexBinary(testEntry.getValue()), retValue);
+                });
+    }
+
+}
